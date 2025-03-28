817e33c53d5c8ac64ca45607a5769daf84a037ec
Into0 <Into0@users.noreply.github.com>
2025-03-29 00:33:13 +0300

    feat: add retry solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -126,8 +126,18 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function retryer() {
+    let error;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch {
+        error += 1;
+      }
+    }
+    throw error;
+  };
 }
 
 /**
